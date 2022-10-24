62971e0e51453eaa2f4ab58144ee340f03c31a1c
Vinicius Floriano <84535923+ViniciusFloriano@users.noreply.github.com>
2022-10-24 15:52:13 -0300

    Delete js directory

--- js/perfil.js
+++
@@ -1,7 +0,0 @@
-function confirmarSenha() {
-    if (senhaAtual == document.getElementById('senha').value && document.getElementById('novasenha').value == document.getElementById('novasenhaconfirma').value) {
-        document.getElementById('enviar').disabled = false;
-    } else {
-        document.getElementById('enviar').disabled = true;
-    }
-}
