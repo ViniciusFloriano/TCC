746c2bbf9e63a17622c66e52cf19b1d49264399d
Vinicius Floriano <84535923+ViniciusFloriano@users.noreply.github.com>
2022-10-31 14:32:29 -0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_set_cookie_params(0);
-    session_start();
-    if(!isset($_SESSION['id']) || $_SESSION['id'] == ''){
-        header("Location:view/usuario/login.php");
-    }
-?>
