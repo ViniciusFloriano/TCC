18380bc25a23d3adec94b14f20e94c24659c69de
Vinicius Floriano <84535923+ViniciusFloriano@users.noreply.github.com>
2022-10-31 14:32:04 -0300

    Delete js directory

--- js/cadastro.js
+++
@@ -1,7 +0,0 @@
-function confirmacao() {
-    if (document.getElementById('senha').value == document.getElementById('senha2').value && document.getElementById('senha').value.length >= 6 && document.getElementById('email').value == document.getElementById('email2').value) {
-        document.getElementById('enviar').disabled = false;
-    } else {
-        document.getElementById('enviar').disabled = true;
-    }
-}
